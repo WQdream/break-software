9e649e3498e44847e1f6daa08eacf8d1a3400caf
wyz <wyz190929@163.com>
2024-05-12 02:36:55 +0800

    修改

---
+++ src/views/about.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        关于我
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'home',
+    components: {
+    },
+    data() {
+        return {
+
+        }
+    }
+
+
+}
+</script>
---
+++ src/views/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        666
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'home',
+    components: {
+    },
+    data() {
+        return {
+
+        }
+    }
+
+
+}
+</script>
